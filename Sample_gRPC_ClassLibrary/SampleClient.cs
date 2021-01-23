278613042a7b608cbdb46534d61b633f0e2e3acf
motoi-tsushima <motoi.tsushima@gmail.com>
2021-01-23 17:31:36 +0900

    プロジェクト ファイルを追加します。

---
+++ Sample_gRPC_ClassLibrary/SampleClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Grpc.Net.Client;
+
+namespace Sample_gRPC_ClassLibrary
+{
+    public class SampleClient
+    {
+        public SampleClient()
+        {
+            this.serviceUrl = "https://localhost:5001";
+            this.grpcChannel = GrpcChannel.ForAddress(this.serviceUrl);
+            this.greeterClient = new Greeter.GreeterClient(this.grpcChannel);
+        }
+
+        private string serviceUrl = null;
+        private GrpcChannel grpcChannel = null;
+        private Greeter.GreeterClient greeterClient = null;
+
+        public GrpcChannel Channel
+        {
+            get { return this.grpcChannel; }
+        }
+
+        public Greeter.GreeterClient GreeterClient
+        {
+            get { return this.greeterClient; }
+        }
+    }
+}
---
+++ Sample_gRPC_WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Grpc.Net.Client;
+using Sample_gRPC_ClassLibrary;
+
+namespace Sample_gRPC_WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ReqestButton_Click(object sender, RoutedEventArgs e)
+        {
+            string requestText = this.ReqestTextBox.Text;
+
+            SampleClient grpcClient = new SampleClient();
+            var reply = grpcClient.GreeterClient.SayHello(new HelloRequest { Name = requestText });
+
+            this.ReplayTextBox.Text = reply.Message;
+        }
+    }
+}
+
