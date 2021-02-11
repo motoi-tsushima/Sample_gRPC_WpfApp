c0ee0eac8ede858ab1629eb304b79f79f967b0be
motoi-tsushima <motoi.tsushima@gmail.com>
2021-02-12 07:14:00 +0900

    1.1.1.2 gRPC 日付型通信 R3

--- Sample_gRPC_WpfApp/MainWindow.xaml.cs
+++ Sample_gRPC_WpfApp/MainWindow.xaml.cs
@@ -79,7 +79,6 @@ namespace Sample_gRPC_WpfApp
             this.SubtractionTextBox.Text = reply.Subtraction.ToString();
             this.MultiplicationTextBox.Text = reply.Multiplication.ToString();
             this.DivisionTextBox.Text = reply.Division.ToString();
-
         }
 
         private Dictionary<string, TimeSpan> _timeZone = null;
@@ -87,25 +86,49 @@ namespace Sample_gRPC_WpfApp
 
         private void InitTimeZone()
         {
-            TimeSpan JapanTimeSpan = new TimeSpan(9, 0, 0);
-            TimeSpan BritainTimeSpan = new TimeSpan(0, 0, 0);
-            TimeSpan AmericaEasternTimeSpan = new TimeSpan(-4, 0, 0);
+            TimeSpan TokyoTimeSpan = new TimeSpan(9, 0, 0);
+            TimeSpan LondonTimeSpan = new TimeSpan(0, 0, 0);
+            TimeSpan New_YorkTimeSpan = new TimeSpan(-5, 0, 0);
+            TimeSpan Los_AngelesTimeSpan = new TimeSpan(-8, 0, 0);
+            TimeSpan AnchorageTimeSpan = new TimeSpan(-9, 0, 0);
+            TimeSpan SingaporeTimeSpan = new TimeSpan(8, 0, 0);
+            TimeSpan TaipeiTimeSpan = new TimeSpan(8, 0, 0);
+            TimeSpan ShanghaiTimeSpan = new TimeSpan(8, 0, 0);
+            TimeSpan SydneyTimeSpan = new TimeSpan(10, 0, 0);
+            TimeSpan YangonTimeSpan = new TimeSpan(6, 30, 0);
+            TimeSpan MoscowTimeSpan = new TimeSpan(4, 0, 0);
+            TimeSpan ParisTimeSpan = new TimeSpan(1, 0, 0);
+            TimeSpan BerlinTimeSpan = new TimeSpan(1, 0, 0);
+            TimeSpan JerusalemTimeSpan = new TimeSpan(2, 0, 0);
 
             this._timeZone = new Dictionary<string, TimeSpan>();
 
-            this._timeZone.Add("Japan", JapanTimeSpan);
-            this._timeZone.Add("Britain", BritainTimeSpan);
-            this._timeZone.Add("AmericaEastern", AmericaEasternTimeSpan);
+            this._timeZone.Add("Tokyo", TokyoTimeSpan);
+            this._timeZone.Add("London", LondonTimeSpan);
+            this._timeZone.Add("New_York", New_YorkTimeSpan);
+            this._timeZone.Add("Los_Angeles", Los_AngelesTimeSpan);
+            this._timeZone.Add("Anchorage", AnchorageTimeSpan);
+            this._timeZone.Add("Singapore", SingaporeTimeSpan);
+            this._timeZone.Add("Taipei", TaipeiTimeSpan);
+            this._timeZone.Add("Shanghai", ShanghaiTimeSpan);
+            this._timeZone.Add("Sydney", SydneyTimeSpan);
+            this._timeZone.Add("Yangon", YangonTimeSpan);
+            this._timeZone.Add("Moscow", MoscowTimeSpan);
+            this._timeZone.Add("Paris", ParisTimeSpan);
+            this._timeZone.Add("Berlin", BerlinTimeSpan);
+            this._timeZone.Add("Jerusalem", JerusalemTimeSpan);
         }
 
         private void ReserveButton_Click(object sender, RoutedEventArgs e)
         {
+            //選択していない場合は無視する。
             if (this.xDatePicker.SelectedDate == null)
                 return;
 
             if (this.xTimeSpanComboBox.SelectedValue == null)
                 return;
 
+            //時間を文字列から数値に変換する。
             int hour, minute, span;
             if(int.TryParse(this.xHourTextBox.Text, out hour) == false) return;
             if (hour > 24) return;
@@ -113,13 +136,20 @@ namespace Sample_gRPC_WpfApp
             if (minute > 60) return;
             if (int.TryParse(this.xSpanTextBox.Text, out span) == false) return;
 
+            //クライアント側のタイムゾーンを設定
             DateTime dateTime = this.xDatePicker.SelectedDate.Value;
             string timeZoneText = ((KeyValuePair<string, TimeSpan>)this.xTimeSpanComboBox.SelectedValue).Key;
             TimeSpan timeZone = ((KeyValuePair<string, TimeSpan>)this.xTimeSpanComboBox.SelectedValue).Value;
+
+            //予約先施設側のタイムゾーンを設定s
+            string countryTimeZoneText = ((KeyValuePair<string, TimeSpan>)this.xCountryTimeSpanComboBox.SelectedValue).Key;
+            TimeSpan countryTimeZone = ((KeyValuePair<string, TimeSpan>)this.xCountryTimeSpanComboBox.SelectedValue).Value;
+
+            //予約時間
             TimeSpan duration = new TimeSpan(0, span, 0);
 
+            //クライアント側のタイムゾーンを反映した日付型を作成する。
             DateTimeOffset dateTimeOffset = new DateTimeOffset(dateTime.Year, dateTime.Month, dateTime.Day, hour, minute, 0, 0, timeZone);
-
 
             //ReservationTime 作成
             ReservationTime reservationTime = new ReservationTime();
@@ -127,16 +157,22 @@ namespace Sample_gRPC_WpfApp
             reservationTime.Time = Google.Protobuf.WellKnownTypes.Timestamp.FromDateTimeOffset(dateTimeOffset);
             reservationTime.Duration = Google.Protobuf.WellKnownTypes.Duration.FromTimeSpan(duration);
             reservationTime.TimeZone = Google.Protobuf.WellKnownTypes.Duration.FromTimeSpan(timeZone);
+            reservationTime.CountryTimeZone = Google.Protobuf.WellKnownTypes.Duration.FromTimeSpan(countryTimeZone);
 
             // gRPC サービスを呼び出す。
             var reply = this.grpcClient.GreeterClient.Reserve(reservationTime);
 
-            // 日付をクライアント側のタイムゾーンに変換する。
+            // リプライ日付をクライアント側のタイムゾーンに変換する。
             DateTimeOffset replyDateTime = reply.Time.ToDateTimeOffset().ToOffset(timeZone);
             DateTimeOffset replyDateTimeOffset = new DateTimeOffset(replyDateTime.DateTime, timeZone);
 
+            DateTimeOffset replyCountryDateTime = reply.Time.ToDateTimeOffset().ToOffset(countryTimeZone);
+
+
             // 予約日を表示する。
-            this.xReserveTextBox.Text = replyDateTimeOffset.ToString("yyyy年MM月dd日 H時m分s秒") + " / " + reply.Duration.ToTimeSpan().ToString() + " / " + reply.TimeZone.ToTimeSpan().ToString(); ;
+            this.xReserveTextBox.Text = replyDateTimeOffset.ToString("yyyy年MM月dd日 H時m分") + " / 時間 = " + reply.Duration.ToTimeSpan().ToString() + " / TimeZone = " + reply.TimeZone.ToTimeSpan().ToString();
+            this.xCountryTextBox.Text = replyCountryDateTime.DateTime.ToString("yyyy年MM月dd日 H時m分") + " / TimeZone = " + countryTimeZone.ToString();
+            this.xReserveUtcTextBox.Text = reply.Time.ToDateTimeOffset().ToString("yyyy年MM月dd日 H時m分");
 
         }
     }
